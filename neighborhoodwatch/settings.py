5a19b191ee71ce2539568f2a7130f86a09724091
careymwarabu <careyakmwarabu@gmail.com>
2021-07-25 17:52:53 +0300

    add settings.py

--- neighborhoodwatch/settings.py
+++ neighborhoodwatch/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'neighboehoodapp.apps.NeighborhoodappConfig',
+    'neighborhoodapp.apps.NeighborhoodappConfig',
 ]
 
 MIDDLEWARE = [
