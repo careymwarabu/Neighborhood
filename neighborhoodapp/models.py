2b162007483736227733673d63a3e42225058064
careymwarabu <careyakmwarabu@gmail.com>
2021-07-25 18:12:23 +0300

    add models.py

--- neighborhoodapp/models.py
+++ neighborhoodapp/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Neighbourhood(models.Model):
     name = models.CharField(max_length= 60)
+    location = models.CharField(max_length=50)
+    count = models.IntegerField(null=False)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
 
     def __str__(self):
         return self.name
