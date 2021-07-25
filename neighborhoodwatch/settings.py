33681fb28a3ad4bf63fbbb54af377c55ad13a74f
careymwarabu <careyakmwarabu@gmail.com>
2021-07-25 17:43:17 +0300

    add settings.py

--- neighborhoodwatch/settings.py
+++ neighborhoodwatch/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'neighborhoodwatch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'neighborhood',
+        'USER': 'carey',
+        'PASSWORD': 'carey',
     }
 }
 
